c4d874fa4d3b10fb7fd40c2edd07c22f9d7cc9a6
sandeep0302 <159161078+sandeep0302@users.noreply.github.com>
2024-07-30 21:59:19 +0530

    restock

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const redux = require('redux');
+const createStore = redux.legacy_createStore; 
+
 const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 
 function orderCake(){
     return{
     type: CAKE_ORDERED,
     quantity:1
+}
+}
+function restockCake(qty = 1){
+    return{
+    type: CAKE_RESTOCKED,
+    quantity:qty
 }
 }
 
@@ -18,7 +28,24 @@ const reducer =(state = initialState, action) => {
             return{
                 numOfCakes:state.numOfCakes -1,
             }
+            case CAKE_RESTOCKED:
+                return{
+                    ...state,
+                    numOfCakes : state.numOfCakes + action.quantity 
+                }
             default:
                 return state
     }
 }
+
+const store = createStore(reducer)  
+
+console.log('Initial state',store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('update state',store.getState()))
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(restockCake(3))
+unsubscribe();
