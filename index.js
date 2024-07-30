df388081c3071891a7117f16f0c690198a22cf45
sandeep0302 <159161078+sandeep0302@users.noreply.github.com>
2024-07-30 14:04:39 +0530

    reducer function

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('From index.js') 
+const CAKE_ORDERED = 'CAKE_ORDERED'
+
+
+function orderCake(){
+    return{
+    type: CAKE_ORDERED,
+    quantity:1
+}
+}
+
+const initialState = {
+    numOfCakes:10,
+}
+
+const reducer =(state = initialState, action) => {
+    switch (action.type){
+        case CAKE_ORDERED:
+            return{
+                numOfCakes:state.numOfCakes -1,
+            }
+            default:
+                return state
+    }
+}
